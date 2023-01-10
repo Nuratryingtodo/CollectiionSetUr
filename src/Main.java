d7cf4e120e644eb48cb7aa06a57173126756bf1f
Nura ttryin to do <foxite78@gmail.com>
2023-01-10 14:30:05 +0600

    Collection open ai

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<String> fruits = new HashSet<String>();
+        fruits.add("арбуз");
+        fruits.add("банан");
+        fruits.add("вишня");
+        fruits.add("груша");
+        fruits.add("дыня");
+        fruits.add("ежевика");
+        fruits.add("женьшень");
+        fruits.add("земляника");
+        fruits.add("ирис");
+        fruits.add("картофель");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
+
+
+
+
+    }
+}
